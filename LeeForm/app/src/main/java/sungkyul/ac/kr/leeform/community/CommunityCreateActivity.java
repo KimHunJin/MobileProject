07d463783c40a098729e51c95c47ef59fe53618c
KimMiSun <haruma5011@hanmail.net>
2016-05-16 22:54:17 +0900

    커뮤니티 작성 아이콘수정, 댓글 아이콘 수정

---
+++ LeeForm/app/src/main/java/sungkyul/ac/kr/leeform/community/CommunityCreateActivity.java
@@ -0,0 +1,40 @@
+package sungkyul.ac.kr.leeform.community;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import sungkyul.ac.kr.leeform.R;
+
+/**
+ * Created by user on 2016-05-16.
+ */
+public class CommunityCreateActivity extends AppCompatActivity{
+
+    ImageView camera,album;
+    private  static int TAKE_PICTURE=1;
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_create);
+        camera=(ImageView)findViewById(R.id.imgCamera);
+        album=(ImageView)findViewById(R.id.imgAlbum);
+
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        album.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
