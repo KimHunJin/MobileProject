1761b1e05b975e4fa41a2c616afe6c6fd05f4d41
yh-kim <btc_yh@naver.com>
2016-05-16 17:54:51 +0900

    drawable-xxhdpi 폴더 추가, knowhow_detail base 추가

---
+++ LeeForm/app/src/main/java/sungkyul/ac/kr/leeform/knowhow/KnowhowDetailActivity.java
@@ -0,0 +1,24 @@
+package sungkyul.ac.kr.leeform.knowhow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import sungkyul.ac.kr.leeform.R;
+
+/**
+ * Created by Kim on 2016-05-16.
+ */
+public class KnowhowDetailActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_knowhow_detail);
+
+        initializeLayout();
+    }
+
+    private void initializeLayout() {
+
+    }
+}
