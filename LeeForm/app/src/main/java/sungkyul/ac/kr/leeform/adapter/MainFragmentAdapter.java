00fe3eb1c4fec74e221d3eb1108eaf906fd9ebd0
KimHunJin <ysg01129@naver.com>
2016-05-16 15:33:56 +0900

    ReadMe.md

--- LeeForm/app/src/main/java/sungkyul/ac/kr/leeform/adapter/MainFragmentAdapter.java
+++ LeeForm/app/src/main/java/sungkyul/ac/kr/leeform/adapter/MainFragmentAdapter.java
@@ -19,7 +19,7 @@ import sungkyul.ac.kr.leeform.fragment.MaterialFragment;
  */
 public class MainFragmentAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 3;
-    private String tabTitles[] = new String[]{"재료", "노하우", "커뮤니티"};
+    private String tabTitles[] = new String[]{"노하우", "재료", "커뮤니티"};
     private Fragment[] fragments = new Fragment[]{new HomeFragment(), new MaterialFragment(), new CommunityFragment()};
     private Context context;
 
--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # MobileProject
 
-Splash
-Intro
+## Done (Layout)
+*Splash
 Sign In
 Sign Up
+Search Password
 
-Home
-Search
+## To Do (Layout)
+*navigation
 Detail List
 Detail View
+
+## Doing (Layout)
+*Material List
+Community
+
+## Hmm...
+*Home
+Search
 Payment View
 Navigation
-
 Kakao Pay
 Google Admob
 Google push Alerm
