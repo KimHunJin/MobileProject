d7d0c71d4655bcf03636c6050b2424e4a7d72110
KimHunJin <ysg01129@naver.com>
2016-06-04 03:39:15 +0900

    splash

---
+++ LeeForm/app/src/main/java/sungkyul/ac/kr/leeform/activity/intro/SplashActivity.java
@@ -0,0 +1,15 @@
+package sungkyul.ac.kr.leeform.activity.intro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sungkyul.ac.kr.leeform.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
