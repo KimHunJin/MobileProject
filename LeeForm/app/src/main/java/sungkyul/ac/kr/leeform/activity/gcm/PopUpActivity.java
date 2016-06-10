a77221d3d0c882c229d757db92e5538d66b99ff3
yh-kim <btc_yh@naver.com>
2016-06-11 01:44:35 +0900

    푸시 팝업 배경 투명하게

--- LeeForm/app/src/main/java/sungkyul/ac/kr/leeform/activity/gcm/PopUpActivity.java
+++ LeeForm/app/src/main/java/sungkyul/ac/kr/leeform/activity/gcm/PopUpActivity.java
@@ -1,20 +1,17 @@
 package sungkyul.ac.kr.leeform.activity.gcm;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import sungkyul.ac.kr.leeform.R;
 import sungkyul.ac.kr.leeform.activity.intro.SplashActivity;
 
-public class PopUpActivity extends AppCompatActivity {
+public class PopUpActivity extends Activity {
 
 
     @Override
